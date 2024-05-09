e2fed88f76070b3593477e33c2f48f6056b8f850
诸葛子房 <xiaoqiu2017wy@163.com>
2024-05-09 16:35:57 +0800

    feat:data

--- api/index.py
+++ api/index.py
@@ -1,7 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request, session,jsonify
 
 app = Flask(__name__)
 
+@app.route('/hello')
+def home():
+    return jsonify({"message": "Hello, World"})
+
+@app.route('/data', methods = ["GET","POST"])   # GET 和 POST 都可以
+def get_data():
+    name = request.args.get("name")
+    age = request.args.get("age")
+    return jsonify({"name": name,"age":age})
 
 @app.route('/')
 def hello():
